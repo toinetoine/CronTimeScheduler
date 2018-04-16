0404b14a5de0683aeaac3c0df9d3ca0c5542eab8
toinetoine <toinetoine@users.noreply.github.com>
2018-04-16 14:51:46 +0800

    Better PullingData example's sleep

--- src/cron/scheduler/examples/PullingData.java
+++ src/cron/scheduler/examples/PullingData.java
@@ -20,30 +20,28 @@ public class PullingData {
 
         // trigger every 1 minute
         CronScheduler cronScheduler = new CronScheduler("* * * * *");
-        long nextPullTime = cronScheduler.getNextTime(TimeUnit.MILLISECONDS);
-
-        while (true) {
-            if (System.currentTimeMillis() >= nextPullTime) {
-                URLConnection connection = url.openConnection();
-                connection.setRequestProperty("Accept-Charset", "UTF-8");
-                connection.setRequestProperty("User-agent", "example");
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                java.util.Scanner scanner = new java.util.Scanner(in).useDelimiter("\\A");
-                if (scanner.hasNext()) {
-                    String responseJson = scanner.next();
-                    JSONObject obj = new JSONObject(responseJson);
-                    JSONArray topPosts = obj.getJSONObject("data").getJSONArray("children");
-                    for (int i = 0; i < topPosts.length(); i++) {
-                        JSONObject post = topPosts.getJSONObject(i).getJSONObject("data");
-                        System.out.println(post.getInt("score") + "\t\t" +
-                                post.getString("title") + "\t\t" + post.getString("url"));
-                    }
+        
+        
+        do {
+            long now = System.currentTimeMillis();
+            long nextPullTime = cronScheduler.getNextTime(TimeUnit.MILLISECONDS);
+            Thread.sleep(nextPullTime - now);
+            
+            URLConnection connection = url.openConnection();
+            connection.setRequestProperty("Accept-Charset", "UTF-8");
+            connection.setRequestProperty("User-agent", "example");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            java.util.Scanner scanner = new java.util.Scanner(in).useDelimiter("\\A");
+            if (scanner.hasNext()) {
+                String responseJson = scanner.next();
+                JSONObject obj = new JSONObject(responseJson);
+                JSONArray topPosts = obj.getJSONObject("data").getJSONArray("children");
+                for (int i = 0; i < topPosts.length(); i++) {
+                    JSONObject post = topPosts.getJSONObject(i).getJSONObject("data");
+                    System.out.println(post.getInt("score") + "\t\t" +
+                            post.getString("title") + "\t\t" + post.getString("url"));
                 }
-
-                nextPullTime = cronScheduler.getNextTime(TimeUnit.MILLISECONDS);
             }
-            Thread.sleep(1000);
-        }
-
+        } while(true);
     }
 }
